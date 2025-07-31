0ef76764e1468398b29fab62247248192cf2caa6
Abdelrahman Alaa <abdelrahmanalaa.psn@gmail.com>
2025-07-31 16:56:34 +0300

    h

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/notes';
+const API_URL = `${import.meta.env.VITE_API_URL}/notes`;
 
 export const fetchNotes = async (setNotes) => {
   try {
